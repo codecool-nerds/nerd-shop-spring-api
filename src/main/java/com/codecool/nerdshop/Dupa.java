8bddf2061e488823e16c083e240b283dfca48d22
Paprok <rafal.becla@gmail.com>
2019-03-06 11:26:44 +0100

    initialized spring repo

---
+++ src/main/java/com/codecool/nerdshop/Dupa.java
@@ -0,0 +1,4 @@
+package com.codecool.nerdshop;
+
+public class Dupa {
+}
---
+++ src/main/java/com/codecool/nerdshop/NerdShopApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.nerdshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerdShopApplication.class, args);
+	}
+
+}
