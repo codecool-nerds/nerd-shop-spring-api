e324a64670b081ce4be8dfd90425fd309c2da950
Paprok <rafal.becla@gmail.com>
2019-03-13 13:40:51 +0100

    fixed parsing model to and from json

--- src/main/java/com/codecool/nerdshop/users/UsersController.java
+++ src/main/java/com/codecool/nerdshop/users/UsersController.java
@@ -1,14 +1,13 @@
 package com.codecool.nerdshop.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api")
 public class UserController {
     private final UserRepository userRepository;
 
@@ -20,5 +19,13 @@ public class UserController {
     @GetMapping("/users")
     public List<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    @PostMapping("/users")
+    public User createNote(@Valid @RequestBody User user) {
+//        Address address = user.getAddress();
+
+        return userRepository.save(user);
+
     }
 }
