2e5f3a134d2693775224bb6dbb69f831a23b7924
Adrian <radian.ost@gmail.com>
2019-03-15 16:31:12 +0100

    add findByProductType method signature to ProductRepository

--- src/main/java/com/codecool/nerdshop/products/ProductRepository.java
+++ src/main/java/com/codecool/nerdshop/products/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.codecool.nerdshop.products;
 
-public interface ProductRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+    List<Product> findByProductType(ProductType productType);
 }
