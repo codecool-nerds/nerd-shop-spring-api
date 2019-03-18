db753930ca90cb2a66bd0736ef93cc45c5f782e2
Paprok <rafal.becla@gmail.com>
2019-03-18 14:30:46 +0100

    comented not working code

--- src/main/java/com/codecool/nerdshop/products/ProductRepository.java
+++ src/main/java/com/codecool/nerdshop/products/ProductRepository.java
@@ -1,9 +1,9 @@
-package com.codecool.nerdshop.products;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-    List<Product> findByProductType(Category category);
-}
+//package com.codecool.nerdshop.products;
+//
+//import org.springframework.data.repository.CrudRepository;
+//
+//import java.util.List;
+//
+//public interface ProductRepository extends CrudRepository<Product, Long> {
+//    List<Product> findByProductType(Category category);
+//}
