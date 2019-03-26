66736c0c266c5d754f1a0c4630ce1d6cb3477d92
Paprok <rafal.becla@gmail.com>
2019-03-26 15:12:30 +0100

    removed unnecesary cors annotation

--- src/main/java/com/codecool/nerdshop/products/ProductController.java
+++ src/main/java/com/codecool/nerdshop/products/ProductController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.http.MediaType;
+
 import java.util.Collection;
 
 @RestController
@@ -15,25 +16,22 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
-    @CrossOrigin(origins = "http://localhost:8081")
+
     @GetMapping
     public Collection<Product> getProductsByCategory(@RequestParam(required = false) String category) {
         return productService.getProductsByCategory(category);
     }
 
-    @CrossOrigin(origins = "http://localhost:8081")
     @GetMapping(value = "/{id}")
     public Product getProductById(@PathVariable long id) {
         return productService.getProductById(id);
     }
 
-    @CrossOrigin(origins = "http://localhost:8081")
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public Product insertProduct(@RequestBody Product product) {
         return productService.insertProduct(product);
     }
 
-    @CrossOrigin(origins = "http://localhost:8081")
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public Product updateProduct(@RequestBody Product product) {
         return productService.updateProduct(product);
