ab49c7cbaf26a61be36380280257f3d6d52d7b13
Paprok <rafal.becla@gmail.com>
2019-03-26 14:52:48 +0100

    changed corsConfig annotation to @Configuration

--- src/main/java/com/codecool/nerdshop/configs/CorsConfig.java
+++ src/main/java/com/codecool/nerdshop/configs/CorsConfig.java
@@ -2,13 +2,13 @@ package com.codecool.nerdshop.configs;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SpringBootApplication
-@RestController
+@Configuration
 public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
