e44dc828079e2a98462ae79e2c4a678c17decdd1
Paprok <rafal.becla@gmail.com>
2019-03-26 13:21:34 +0100

    enabled dataLoader

--- src/main/java/com/codecool/nerdshop/DataLoader.java
+++ src/main/java/com/codecool/nerdshop/DataLoader.java
@@ -15,33 +15,33 @@ import java.util.ArrayList;
 import java.util.List;
 @Component
 public class DataLoader {
-//    @Bean
-//    public List<User> loadExampleUsers(UserRepository repository) {
-//        List<User> list = new ArrayList<>();
-//        User admin = new User("adrian@nerd.pl", "admin123", AccessLevel.ADMIN);
-//        repository.save(admin);
-//        list.add(admin);
-//        User user = new User("karol.trzaska@nerd.pl", "wTibie", AccessLevel.USER);
-//        repository.save(user);
-//        list.add(user);
-//        return list;
-//    }
-//
-//    @Bean
-//    public List<Product> loadCategoriesAndProducts(CategoryRepository categoryRepo, ProductRepository productRepo) {
-//        List<Product> products = new ArrayList<>();
-//        Category mice = new Category("MICE");
-//        categoryRepo.save(mice);
-//        Category keyboards = new Category("KEYBOARDS");
-//        categoryRepo.save(keyboards);
-//
-//        Product steel = new Product("Rival 300", mice, 150.00, "cool steelseries");
-//        Product razer = new Product("Razer 2000", keyboards, 300.00, "random razer keyboard");
-//        products.add(steel);
-//        products.add(razer);
-//        productRepo.saveAll(products);
-//        return products;
-//    }
+    @Bean
+    public List<User> loadExampleUsers(UserRepository repository) {
+        List<User> list = new ArrayList<>();
+        User admin = new User("adrian@nerd.pl", "admin123", AccessLevel.ADMIN);
+        repository.save(admin);
+        list.add(admin);
+        User user = new User("karol.trzaska@nerd.pl", "wTibie", AccessLevel.USER);
+        repository.save(user);
+        list.add(user);
+        return list;
+    }
+
+    @Bean
+    public List<Product> loadCategoriesAndProducts(CategoryRepository categoryRepo, ProductRepository productRepo) {
+        List<Product> products = new ArrayList<>();
+        Category mice = new Category("MICE");
+        categoryRepo.save(mice);
+        Category keyboards = new Category("KEYBOARDS");
+        categoryRepo.save(keyboards);
+
+        Product steel = new Product("Rival 300", mice, 150.00, "cool steelseries");
+        Product razer = new Product("Razer 2000", keyboards, 300.00, "random razer keyboard");
+        products.add(steel);
+        products.add(razer);
+        productRepo.saveAll(products);
+        return products;
+    }
 
 
 }
